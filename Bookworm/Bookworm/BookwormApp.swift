687c54211500799d31b63facd59cc07897ba05de
Parmesh Yadav <parmesh20319@iiitd.ac.in>
2025-09-23 11:16:46 +0530

    till day 53

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Parmesh Yadav on 23/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
