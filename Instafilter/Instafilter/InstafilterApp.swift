10c37d2f511e7662de44cd4b0bd678ff376900c1
Parmesh Yadav <parmesh20319@iiitd.ac.in>
2025-10-03 06:59:22 +0530

    till day 62

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Parmesh Yadav on 03/10/25.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
