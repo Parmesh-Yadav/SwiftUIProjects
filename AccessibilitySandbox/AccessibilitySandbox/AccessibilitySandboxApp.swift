5a9b75f25c5cffe2cd2146b89f0f963d4382e5f7
Parmesh Yadav <parmesh20319@iiitd.ac.in>
2025-10-18 13:03:17 +0530

    till day 74

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Parmesh Yadav on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
