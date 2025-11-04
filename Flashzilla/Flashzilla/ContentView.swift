7486973e004af09205cbb5bd558bb1d7cc274dfd
Parmesh Yadav <parmesh20319@iiitd.ac.in>
2025-11-04 11:15:42 +0530

    till day 87

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -7,23 +7,26 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    
-    
-    var body: some View {
-        VStack {
-            Text("Hello")
-            
-            Spacer()
-                .frame(height: 100)
-            
-            Text("World")
-        }
-        .contentShape(.rect)
-        .onTapGesture {
-            print("VStack tapped")
-        }
+func withOptionalAnimation<Result>(_ animation: Animation? = .default,_ body: () throws -> Result) rethrows -> Result {
+    if UIAccessibility.isReduceMotionEnabled {
+        return try body()
+    } else {
+        return try withAnimation(animation, body)
     }
+}
+
+struct ContentView: View {
+    @Environment(\.accessibilityReduceTransparency) var accessibilityReduceTransparency
+    @State private var scale = 1.0
+
+    var body: some View {
+        Text("Hello World")
+            .padding()
+            .background(accessibilityReduceTransparency ? .black : .black.opacity(0.5))
+            .foregroundStyle(.white)
+            .clipShape(.capsule)
+    }
+    
 }
 
 #Preview {
