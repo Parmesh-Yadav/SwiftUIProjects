8c17b366d34262d94ca4c20bf3650ee9d5f3bfef
Parmesh Yadav <parmesh20319@iiitd.ac.in>
2025-11-05 11:28:35 +0530

    till day 88

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Parmesh Yadav on 05/11/25.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
