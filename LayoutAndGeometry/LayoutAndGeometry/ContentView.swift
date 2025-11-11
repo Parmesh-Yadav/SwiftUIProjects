c8cf6166f49161b02bd378c55bae138e26abdcbd
Parmesh Yadav <parmesh20319@iiitd.ac.in>
2025-11-11 11:37:54 +0530

    till day 93

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,11 +9,31 @@ import SwiftUI
 
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
     var body: some View {
-        Text("Hello, world!")
-            .background(.blue)
-            .offset(x: 100, y: 100)
-            .background(.red)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { number in
+                    
+                    Text("Number \(number)")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(.red)
+                        .frame(width: 200, height: 200)
+                        .visualEffect { content, proxy in
+                            content
+                                .rotation3DEffect(
+                                    .degrees(-proxy.frame(in: .global).minX / 8),
+                                    axis: (x:0, y:1, z:0)
+                                )
+                        }
+
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(.viewAligned)
     }
 }
 
