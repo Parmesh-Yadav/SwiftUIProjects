8f53934b601c533d371400a54e2c0ff274e0c3cd
Parmesh Yadav <parmesh20319@iiitd.ac.in>
2025-11-15 13:13:52 +0530

    till day 96

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Parmesh Yadav on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnowSeeker/SnowSeekerTests/SnowSeekerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerTests.swift
+//  SnowSeekerTests
+//
+//  Created by Parmesh Yadav on 15/11/25.
+//
+
+import Testing
+@testable import SnowSeeker
+
+struct SnowSeekerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
