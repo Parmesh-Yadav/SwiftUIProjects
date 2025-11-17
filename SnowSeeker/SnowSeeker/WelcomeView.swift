ebf5e346927a0496b1b40f0b4b97501a86dbd038
Parmesh Yadav <parmesh20319@iiitd.ac.in>
2025-11-17 20:48:34 +0530

    till day 97

---
+++ SnowSeeker/SnowSeeker/WelcomeView.swift
@@ -0,0 +1,25 @@
+//
+//  WelcomeView.swift
+//  SnowSeeker
+//
+//  Created by Parmesh Yadav on 17/11/25.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to SnowSeeker")
+                .font(.largeTitle)
+            
+            Text("Please select a resort from the left-hand menu; swipe from the edge to show it.")
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.center)
+        }
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
