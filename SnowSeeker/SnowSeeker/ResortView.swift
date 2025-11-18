4fc5833c36337901847288151da9cf28c87c376d
Parmesh Yadav <parmesh20319@iiitd.ac.in>
2025-11-18 21:32:39 +0530

    till day 98

--- SnowSeeker/SnowSeeker/ResortView.swift
+++ SnowSeeker/SnowSeeker/ResortView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct ResortView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
+    @Environment(Favorites.self) var favorites
+    
+    @State private var selectedFacility: Facility?
+    @State private var showingFacility = false
+    
     let resort: Resort
     
     var body: some View {
@@ -18,12 +25,20 @@ struct ResortView: View {
                     .scaledToFit()
                 
                 HStack {
-                    ResortDetailsView(resort: resort)
+                    if horizontalSizeClass == .compact && dynamicTypeSize > .large {
+                        VStack(spacing: 10) { ResortDetailsView(resort: resort) }
+                        
+                        VStack(spacing: 10) { SkiDetailsView(resort: resort) }
+                    } else {
+                        ResortDetailsView(resort: resort)
+                        
+                        SkiDetailsView(resort: resort)
+                    }
                     
-                    SkiDetailsView(resort: resort)
                 }
                 .padding(.vertical)
                 .background(.primary.opacity(0.1))
+                .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
                 
                 Group {
                     Text(resort.description)
@@ -32,17 +47,42 @@ struct ResortView: View {
                     Text("Facilites")
                         .font(.headline)
                     
-                    Text(resort.facilities, format: .list(type: .and))
-                        .padding(.vertical)
+                    HStack {
+                        ForEach(resort.facilityTypes) { facility in
+                            Button {
+                                selectedFacility = facility
+                                showingFacility = true
+                            }label: {
+                                facility.icon
+                                    .font(.title)
+                            }
+                        }
+                    }
+                    .padding(.vertical)
                 }
                 .padding(.horizontal)
+                
+                Button(favorites.contains(resort) ? "Remove from favorites" : "Add to favorites") {
+                    if favorites.contains(resort) {
+                        favorites.remove(resort)
+                    } else {
+                        favorites.add(resort)
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                .padding()
             }
         }
         .navigationTitle("\(resort.name), \(resort.country)")
         .navigationBarTitleDisplayMode(.inline)
+        .alert(selectedFacility?.name ?? "More information", isPresented: $showingFacility, presenting: selectedFacility) { _ in
+        } message: { facility in
+            Text(facility.description)
+        }
     }
 }
 
 #Preview {
     ResortView(resort: Resort.example)
+        .environment(Favorites())
 }
